e4bdc122fdc6bf9f4f68419f3182eabd793af510
Laurent Cetinsoy <lce@nowledgeable.com>
2023-09-15 22:44:00 +0200

    task planner implementation based on llm_completer interface

---
+++ src/LLMCompleter.py
@@ -0,0 +1,9 @@
+
+
+import abc
+
+
+class LLMCompleter:
+    
+    @abc.abstractmethod
+    def complete(self, prompt:str, max_token:int, temperature:float) -> str: pass
--- src/TaskPlanner.py
+++ src/TaskPlanner.py
@@ -1,7 +1,12 @@
 from typing import List
 from Skills import Skill
+from LLMCompleter import LLMCompleter
 
 class TaskPlanner:
+    
+    def __init__(self, llm_completer:LLMCompleter):
+        
+        self.llm_completer = llm_completer
     
     def get_next_task(
             self,
@@ -10,5 +15,17 @@ class TaskPlanner:
             
     ) -> Skill:
         
-        return skills[0]
+        skill_list = [f'{i} {skill.name}:{skill.description}' for skill,i in enumerate(skills)]
+        
+        prompt = f"""
+            You have the following goal: {general_goal}
+            You have the follwoing skills: {skill_list}
+            
+            return the next step to do as a number.
+            The skill number is:
+        """
+        
+        i_skill = self.llm_completer.complete(prompt, 1000, 0.5)
+        
+        return skills[int(i_skill)]
     
