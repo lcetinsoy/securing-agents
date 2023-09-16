09a424131c167df0e32d55d0e820a72b4b25e8f1
Laurent Cetinsoy <lce@nowledgeable.com>
2023-09-16 19:53:09 +0200

    adding OpenAI LLM completer adapter

---
+++ src/OpenApiLLMAdapter.py
@@ -0,0 +1,23 @@
+import openai
+
+from dotenv import load_dotenv,get_key
+from LLMCompleter import LLMCompleter
+
+class OpenApiLLMAdapter(LLMCompleter):
+    
+    def __init__(self):
+        load_dotenv()
+        self.key = get_key('OPEN_API_KEY')
+        openai.api_key = self.key
+        self.model ="davinci-002"
+        
+    def complete(self, prompt: str, max_token: int, temperature: float) -> str:
+        
+        completion = openai.Completion.create(
+            model=self.model,
+            temperature=temperature,
+            max_token=max_token,
+            prompt=prompt
+        )
+        
+        return completion.choices[0].text
